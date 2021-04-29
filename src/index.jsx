0c0df52bee3c8845fd8d8a258a495b65363ee5dd
Alexei Mukovozchik <krisfragerfeed@gmail.com>
2021-04-29 18:28:11 +0300

    init: added source files

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const template = React.createElement('h1', null, 'React Counter!');
+
+ReactDOM.render(template, document.getElementById('root'));
