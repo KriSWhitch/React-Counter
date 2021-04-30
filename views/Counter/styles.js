da53774fcd3e11c52d052dfcfc89912cc5907639
Alexei Mukovozchik <krisfragerfeed@gmail.com>
2021-04-30 18:21:12 +0300

    feat: putting the repeating style into a separate variable

--- views/Counter/styles.js
+++ views/Counter/styles.js
@@ -6,27 +6,27 @@ const displayFlex = {
 
 const styles = {
   button: {
-    borderRadius: 3,
     height: 48,
-    padding: '0 10px',
     margin: 10,
+    padding: '0 10px',
+    borderRadius: 3,
   },
   counter: {
     ...displayFlex,
     flexDirection: 'column',
   },
   counter_display: {
-    textAlign: 'center',
     ...displayFlex,
+    width: '500px',
+    height: '200px',
+    textAlign: 'center',
     border: '1px solid black',
     borderTopLeftRadius: 3,
     borderTopRightRadius: 3,
-    width: '500px',
-    height: '200px',
   },
   counter_control_panel: {
-    width: '500px',
     ...displayFlex,
+    width: '500px',
     border: '1px solid black',
     borderBottomLeftRadius: 3,
     borderBottomRightRadius: 3,
